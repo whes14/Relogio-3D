fd9b7199e932ecaf52c46dc7cb2c08b2315f1536
Wesley Bellinatti Ruiz <wesley.ruiz@pmpf.local>
2022-11-17 15:23:42 -0300

    Inclusão relogio

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Relogio Analógico</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+	<div class="container">
+		<div class="clock">
+			<div class="hr" id="hr"></div>
+			<div class="min" id="min"></div>
+			<div class="sec" id="sec"></div>
+		</div>
+	</div>
+	<script src="working.js"></script>
+</body>
+</html>
